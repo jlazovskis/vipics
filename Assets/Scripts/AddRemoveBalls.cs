8d79bfe5d3086c51643db8acde1ccbf86599fc86
Janis Lazovskis <jlazovskis@gmail.com>
2019-01-23 19:17:51 -0600

    ball experiments, cant add yet

---
+++ Assets/Scripts/AddRemoveBalls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRemoveBalls : MonoBehaviour {
+    public GameObject leftHandAnchor;
+    public GameObject rightHandAnchor;
+    public Transform ballPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.RawButton.B)) {
+            if (rightHandAnchor != null) {
+                Instantiate(ballPrefab, rightHandAnchor.transform.position, rightHandAnchor.transform.rotation);
+            }
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.Y)) {
+            if (leftHandAnchor != null) {
+                Instantiate(ballPrefab, leftHandAnchor.transform.position, leftHandAnchor.transform.rotation);
+            }
+        }
+    }
+}
