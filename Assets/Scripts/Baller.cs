d41db2844cc11a8eacad0e57351fe8f6e7ec0e47
Janis Lazovskis <jlazovskis@gmail.com>
2019-01-28 14:43:00 -0600

    now deletes just mesh. ball stil exists

--- Assets/Scripts/Baller.cs
+++ Assets/Scripts/Baller.cs
@@ -8,6 +8,9 @@ public class Baller : MonoBehaviour {
     public GameObject ballPrefab;
 	public float threshold = 0.03f;	
 	private GameObject nearBall;
+	private GameObject clone;
+	private string name;
+	static int num;
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,12 +20,18 @@ public class Baller : MonoBehaviour {
         // Create
         if (OVRInput.GetDown(OVRInput.RawButton.B)) {
             if (rightHandAnchor != null) {
-                Instantiate(ballPrefab, rightHandAnchor.transform.position, rightHandAnchor.transform.rotation);
+                GameObject clone = (GameObject)Instantiate(ballPrefab, rightHandAnchor.transform.position, rightHandAnchor.transform.rotation);
+				clone.name = "clone" + num.ToString();
+				num++;
+				// BoxCollider bc = gameObject.AddComponent(typeof(BoxCollider)) as BoxCollider;
+				// Debug.Log("FIND: " + clone.GetComponent<BoxCollider>());
             }
         }
         if (OVRInput.GetDown(OVRInput.RawButton.Y)) {
             if (leftHandAnchor != null) {
-                Instantiate(ballPrefab, leftHandAnchor.transform.position, leftHandAnchor.transform.rotation);
+				GameObject clone = (GameObject)Instantiate(ballPrefab, leftHandAnchor.transform.position, leftHandAnchor.transform.rotation);
+				clone.name = "clone" + num.ToString();
+				num++;
             }
         }
 		
@@ -31,18 +40,32 @@ public class Baller : MonoBehaviour {
 				RaycastHit[] hits = Physics.SphereCastAll(rightHandAnchor.transform.position, threshold, rightHandAnchor.transform.forward, 0f);
 				foreach (RaycastHit ball in hits){
 					// Debug.Log("Text: BALLS" + ball);
-					nearBall = ball.transform.gameObject;
-					Debug.Log("GONNA DESTROY: " + nearBall.tag + " " + nearBall);
-					Destroy(nearBall);
+					nearBall = ball.collider.gameObject;
+					Debug.Log("GONNA DESTROY: " + ball.collider.name);
+					// Debug.Log(GameObject.Find(nearBall.name));
+					// name = ball.transform.name;
+					// Destroy(GameObject.Find(ball.collider.name));
 					// if (nearBall.tag == "BALL") {
 						// Destroy(nearBall);
-						// if (ball.collider != null ) {
+					// if (gameObject != null) {
+						// Destroy(gameObject);
+					// }
 							// Debug.Log("Text: AFTER" + nearBall.tag);
-						// Destroy(nearBall.GetComponent<MeshRenderer>());
-							// Destroy(ball.rigidbody);
+					Destroy(nearBall.GetComponent<MeshRenderer>());
+					// Destroy(ball.rigidbody);
 							// Destroy(ball.transform.gameObject);
 						// }
 					// }
+				}
+			}
+		}
+		if (OVRInput.GetDown(OVRInput.RawButton.X)) {
+			if (leftHandAnchor != null) {
+				RaycastHit[] hits = Physics.SphereCastAll(leftHandAnchor.transform.position, threshold, leftHandAnchor.transform.forward, 0f);
+				foreach (RaycastHit ball in hits){
+					nearBall = ball.collider.gameObject;
+					Debug.Log("GONNA DESTROY: " + ball.collider.name);
+					Destroy(nearBall.GetComponent<MeshRenderer>());
 				}
 			}
 		}
