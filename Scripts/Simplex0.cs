792d298223a25842a9ffd8c23d0d6a0ed2457eee
Janis Lazovskis <jlazovskis@gmail.com>
2019-04-17 20:58:58 -0500

    made new script for balls and edges. Baller not changed yet to accomodate for this

---
+++ Scripts/Simplex0.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 0-dimensional (ball, or vertex) class
+public class Simplex0 : MonoBehaviour {
+	
+    public GameObject ballPrefab;
+    public GameObject haloPrefab;	
+	
+	private int index;
+
+	private GameObject simplex;
+	private List<Simplex1> neighbors1 = new List<Simplex1>();
+
+	private GameObject halo;
+	private float r = 0.1f;
+	
+	public Simplex0(GameObject anchor, int num) {
+		index = num;
+		simplex = (GameObject)Instantiate(ballPrefab, anchor.transform.position, anchor.transform.rotation);
+		simplex.name = "clone" + num.ToString();
+		halo = (GameObject)Instantiate(haloPrefab, anchor.transform.position, anchor.transform.rotation);
+		halo.name = "halo" + num.ToString();
+		halo.transform.SetParent(simplex.transform);
+		//neighbors1 = CylinderList;
+	}
+}
---
+++ Scripts/Simplex1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1-dimensional (cylinder, or edge) class
+public class Simplex1 : MonoBehaviour {
+	
+    public GameObject cylinderPrefab;
+	
+	private int index;
+	private GameObject simplex;
+	private float length;
+	private List<Simplex0> neighbors0 = new List<Simplex0>();
+
+	public Simplex1(GameObject ballA, GameObject ballB, int num) {
+		index = num;
+		simplex = (GameObject)Instantiate(cylinderPrefab, (ballA.transform.position + ballB.transform.position)/2, Quaternion.FromToRotation(Vector3.up, ballA.transform.position - ballB.transform.position));
+		simplex.transform.localScale = new Vector3(0.015f, Vector3.Distance(ballA.transform.position/2, ballB.transform.position/2), 0.015f);
+		simplex.name = "cylinder" + num.ToString();
+		//neighbors0 = BallList;
+	}		
+}
